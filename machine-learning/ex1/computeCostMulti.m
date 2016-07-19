0dccf63f7b8d797d3c38c908b091610defa4dd80
Calvin <calvinoh@gmail.com>
2016-07-18 23:43:04 -0700

    update ex1 code

--- machine-learning/ex1/computeCostMulti.m
+++ machine-learning/ex1/computeCostMulti.m
@@ -6,7 +6,7 @@ function J = computeCostMulti(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
@@ -14,7 +14,11 @@ J = 0;
 %               You should set J to the cost.
 
 
-
+h = X * theta;
+error = h - y;
+error_sqr = error .^ 2;
+batch = sum(error_sqr);
+J = 1/(2*m) * batch;
 
 
 % =========================================================================
